c139d2bcca0c9e2abc4f518541fb6e0933a67617
Martin Schubert <mfschubert@gmail.com>
2025-04-10 10:59:19 -0700

    Version updated from v0.0.0 to v0.0.1

--- src/ccmaps/__init__.py
+++ src/ccmaps/__init__.py
@@ -1,6 +1,6 @@
 """ccmaps: A few custom colormaps for matplotlib."""
 
-__version__ = "v0.0.0"
+__version__ = "v0.0.1"
 __author__ = "Martin F. Schubert <mfschubert@gmail.com>"
 
 from .ccmaps import cmap_for_wavelength as cmap_for_wavelength
